a8f64ca660963e08819b4cf7d67c76e3de7f6fd0
unfound <448217185@qq.com>
2022-04-07 21:26:54 +0800

    feat: 完成课程详情页

--- packages/bff/index.js
+++ packages/bff/index.js
@@ -2,15 +2,33 @@ const Koa = require('koa')
 const mount = require('koa-mount')
 const static = require('koa-static')
 const fs = require('fs')
+const rpcClient = require('./client')
+const createTemplate = require('./template')
+
+const template = createTemplate(`${__dirname}/template/index.html`)
 
 const app = new Koa()
 
-app.use(
-    static(__dirname + '/source/')
-)
+app.use(mount('/static', static(`${__dirname}/source/static/`)))
 
-app.use(mount('/', async (ctx) => {
-    ctx.body = fs.readFileSync(__dirname + '/source/index.html', 'utf-8')
-}));
+app.use(async (ctx) => {
+    if (!ctx.query.columnid) {
+        ctx.status = 400
+        ctx.body = 'invalid columnid'
+        return
+    }
+
+    const result = await new Promise((resolve, reject) => {
+        rpcClient.write({
+            columnid: ctx.query.columnid
+        }, function (err, data) {
+            err ? reject(err) : resolve(data)
+        })
+    })
+
+    ctx.status = 200
+    ctx.body = template(result)
+    
+})
 
 app.listen(3000)
